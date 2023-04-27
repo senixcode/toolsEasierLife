3a1872813f538622e64a6a4c547dea0738f8274d
Luis Romero <codesenix@gmail.com>
2023-04-27 12:58:21 -0500

    add useModal

--- src/core/providers/ModalProvider.jsx
+++ src/core/providers/ModalProvider.jsx
@@ -1,36 +1,14 @@
 import React from 'react'
-import ModalContext, { initialContext } from '../context/ModalContext'
-import { useState } from 'react'
-import { ACTIONS_MODALS } from '../const/actionsModal.cons'
+import Modal from 'react-modal'
+import ModalContext from '../context/ModalContext'
+import useModal from '../hooks/useModal'
+
+Modal.setAppElement("#root")
 
 function ModalProvider({ children }) {
-    const [modalState, setModalState] = useState(initialContext)
-    const changeActionsModal = (action) =>
-        setModalState({
-            open: !modalState.open,
-            payload: {
-                actions: {
-                    ...initialContext.payload.actions,
-                    [action]: !modalState.payload.actions[action]
-                }
-            }
-        })
-
-
-    const handleRegisterModal = () => changeActionsModal(ACTIONS_MODALS.register)
-    const handleEditerModal = () => changeActionsModal(ACTIONS_MODALS.editer)
-
-
-    const closeModal = () => setModalState(initialContext)
-
+    const params = useModal()
     return (
-        <ModalContext.Provider value={{
-            modalState,
-            changeActionsModal,
-            handleRegisterModal,
-            handleEditerModal,
-            closeModal
-        }}>
+        <ModalContext.Provider value={params}>
             {children}
         </ModalContext.Provider>
     )
