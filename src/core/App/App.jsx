26339b5427c83c87e0cab65395f22c6ab3e25cc1
Luis Romero <codesenix@gmail.com>
2023-04-27 12:57:55 -0500

    change ModalProvider and add TanstackProvider

--- src/core/App/App.jsx
+++ src/core/App/App.jsx
@@ -3,19 +3,18 @@
 import PersonalAccountant from '../../domains/personal-accountant/presentation/pages/PersonalAccountant'
 // imports providers
 import ModalProvider from '../providers/ModalProvider'
-//imports Libreries
-import Modal from 'react-modal'
 //import styles
 import './app.css'
-
-Modal.setAppElement("#root")
+import TanstackProvider from '../providers/TanstackProvider'
 
 function App() {
 
   return (
-    <ModalProvider>
-      <PersonalAccountant />
-    </ModalProvider>
+    <TanstackProvider>
+      <ModalProvider>
+        <PersonalAccountant />
+      </ModalProvider>
+    </TanstackProvider>
   )
 }
 
