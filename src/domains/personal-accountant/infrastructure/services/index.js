3a6d99263639d0d5cc7b310fb5cb3723effc4c75
Luis Romero <codesenix@gmail.com>
2023-04-30 01:30:06 -0500

    add axios headers

--- src/domains/personal-accountant/infrastructure/services/index.js
+++ src/domains/personal-accountant/infrastructure/services/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
     baseURL: 'http://localhost:3000/',
     timeout: 1000,
-    headers: { 'X-Custom-Header': 'foobar' }
+    headers: { 'Accept-Language': 'application/json' }
 })
 
 export default instance
