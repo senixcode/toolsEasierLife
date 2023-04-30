426ca405e7aa3da16b0f5ac1d01d0cbfab56097d
Luis Romero <codesenix@gmail.com>
2023-04-30 01:28:59 -0500

    add service create Detail

--- src/domains/personal-accountant/infrastructure/services/detail/Detail.service.js
+++ src/domains/personal-accountant/infrastructure/services/detail/Detail.service.js
@@ -1,15 +1,37 @@
 import instance from "..";
-import detailAdapter from "../../adapters/Detail.adapter";
+import detailInputAdapter from "../../adapters/detailInput.adapter";
+import detailOutputAdapter from "../../adapters/detailOutput.adapter";
+import detailValidateAdapter, { messageRequirements } from "../../adapters/detailValidate.adapter";
 
 class DetailService {
-    #path 
-    constructor(){
+    #path
+    constructor() {
         this.#path = 'details'
     }
 
-    async list(){
-       const getList = await instance.get(`/${this.#path}`)
-       return detailAdapter(getList.data)
+    async list() {
+        try {
+            const result = await instance.get(`/${this.#path}`)
+            return detailOutputAdapter(result.data)
+        } catch (error) {
+            console.log("DetailService().list()", error);
+        }
+    }
+
+    async create(body) {
+        try {
+            console.log("detailValidateAdapter: ", detailValidateAdapter(body))
+            if (detailValidateAdapter(body)) {
+                new Error(`Error requirements params ${messageRequirements}`)
+            }
+            const bodyFormater = detailInputAdapter(body)
+            console.log("bodyFormater: ", bodyFormater)
+            const result = await instance.post(`/${this.#path}`,
+                bodyFormater)
+            return detailOutputAdapter(result.data)
+        } catch (error) {
+            console.log("DetailService().create()", error);
+        }
     }
 
 }
