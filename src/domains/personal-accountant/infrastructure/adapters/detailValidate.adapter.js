a6667bf5b925cc7dce6176d4a5ad309f64cdf12f
Luis Romero <codesenix@gmail.com>
2023-04-30 01:28:41 -0500

    add adapters for details

---
+++ src/domains/personal-accountant/infrastructure/adapters/detailValidate.adapter.js
@@ -0,0 +1,17 @@
+import Detail from "../models/Details"
+
+
+const detailValidateAdapter = (body) => {
+    if (!body[Detail.name] ||
+        !body[Detail.detailType] ||
+        !body[Detail.amountOfMoney]) {
+        return true
+    }
+
+    return false
+
+}
+
+export const messageRequirements = `${Detail.name}, ${Detail.detailType}, ${Detail.amountOfMoney}`
+
+export default detailValidateAdapter
