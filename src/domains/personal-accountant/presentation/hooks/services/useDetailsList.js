49d791548706397aa2232cb1a4c8790a9a5693e6
Luis Romero <codesenix@gmail.com>
2023-05-01 17:13:21 -0500

    add useDetailsList

--- src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
+++ src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query"
 import DetailService from "../../../infrastructure/services/detail/Detail.service"
 
 function useDetailsList() {
-    const { status, data: details, error } = useQuery({
-        queryKey: ['detailservice'],
+    const { status, data: details, error, refetch } = useQuery({
+        queryKey: ['detailsList'],
         queryFn: () => new DetailService().list(),
+        refetchInterval: 1000,
     })
 
-    return { status, details, error }
+    return { status, details, error, refetch }
 }
 
 export default useDetailsList
