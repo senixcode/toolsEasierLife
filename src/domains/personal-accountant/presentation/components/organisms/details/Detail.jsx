78ec1fea98d62a29fbaa5a13f38b314e4cf08c8d
Luis Romero <codesenix@gmail.com>
2023-05-01 17:14:25 -0500

    use DetailProvider

--- src/domains/personal-accountant/presentation/components/organisms/details/Detail.jsx
+++ src/domains/personal-accountant/presentation/components/organisms/details/Detail.jsx
@@ -2,10 +2,12 @@ import CardDetail from "../../molecules/card-detail/CardDetail";
 import CardSkeleton from "../../molecules/card-skeleton/CardSkeleton";
 import { createListMock } from "../../../../../../shared/infrastructure/adapters/createListMock";
 import useDetailsList from "../../../hooks/services/useDetailsList";
+import { useContext } from "react";
+import DetailContext from "../../../context/DetailContext";
 
 
 function Detail() {
-  const { status, details, error } = useDetailsList()
+  const { status, details, error } = useContext(DetailContext)
 
   if (status === 'error') return <span>Error: {error.message}</span>
 
--- src/domains/personal-accountant/presentation/pages/PersonalAccountant.jsx
+++ src/domains/personal-accountant/presentation/pages/PersonalAccountant.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import Search from '../components/organisms/search/Search';
 import TemplateSaveDetail from '../components/templates/modal-save-detail/TemplateSaveDetail';
 import Detail from '../components/organisms/details/Detail';
+import DetailProvider from '../providers/DetailProvider';
 
 function PersonalAccountant() {
     
 
     return (
-        <div className="App">
-            <TemplateSaveDetail />
-            <div className='flex-center'>
-                <h1 className='title'>Personal Accountant</h1>
-            </div>
-            <Search  />
-            <Detail  />
-        </div>
+        <DetailProvider>
+            <main>
+                <TemplateSaveDetail />
+                <div className='flex-center'>
+                    <h1 className='title'>Personal Accountant</h1>
+                </div>
+                <Search />
+                <Detail />
+            </main>
+        </DetailProvider>
     )
 }
 
