038f22b18b42fd068c3e26cbbf20ef9aede087be
Luis Romero <codesenix@gmail.com>
2023-05-02 06:33:11 -0500

    implementation search by name

--- src/domains/personal-accountant/presentation/components/organisms/search/Search.jsx
+++ src/domains/personal-accountant/presentation/components/organisms/search/Search.jsx
@@ -3,11 +3,19 @@ import "./search.css"
 import IconCustom from '../../atoms/iconcustom/IconCustom';
 import { useContext } from 'react';
 import ModalContext from '../../../../../../core/context/ModalContext';
+import useDetailSearch from '../../../hooks/services/useDetailSearch';
 function Search() {
     const { handleRegisterModal } = useContext(ModalContext)
+    const { onChange, searchDetail } = useDetailSearch()
     return (
         <section className='section-header'>
-            <input className='search__input' type="search" name="" placeholder='search by name and detail type' id="" />
+            <input 
+                className='search__input'
+                type="search"
+                onChange={onChange}
+                placeholder='search by name'
+                onKeyDown={searchDetail}
+            />
             <IconCustom btnClass="primary button-add" ciconClass='icon-add' icon={icon.cilPlus} ciconOnclick={handleRegisterModal} />
         </section>
     )
