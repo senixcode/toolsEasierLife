3ca7bc51286c66a08fc00d3827f22227ca6e6568
Luis Romero <codesenix@gmail.com>
2023-05-02 06:32:21 -0500

    add useState details

--- src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
+++ src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
@@ -1,16 +1,22 @@
+import { useEffect, useState } from "react"
 import { useQuery } from "@tanstack/react-query"
 import DetailService from "../../../infrastructure/services/detail/Detail.service"
-import { useState } from "react"
 
 function useDetailsList() {
-    const { status, data: details, error, isFetching, refetch } = useQuery({
+    const [details, setDetails] = useState([])
+    const { status, data, error, isFetching, refetch } = useQuery({
         queryKey: ['detailsList'],
         queryFn: () => new DetailService().list(),
     })
+    const resetDetails = () => setDetails(data)
+
+    useEffect(() => {
+        resetDetails()
+    }, [data])
 
     const refresh = () => setTimeout(function () { refetch() }, 1000)
-    
-    return { status, details, error, isFetching, refresh }
+
+    return { status, details, setDetails, error, isFetching, refresh, resetDetails }
 }
 
 export default useDetailsList
