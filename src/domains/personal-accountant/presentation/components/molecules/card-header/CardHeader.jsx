70f79e393940df27c5646fbe7c8e84158384b323
Luis Romero <codesenix@gmail.com>
2023-05-02 05:04:29 -0500

    add handleEditerModal

--- src/domains/personal-accountant/presentation/components/molecules/card-header/CardHeader.jsx
+++ src/domains/personal-accountant/presentation/components/molecules/card-header/CardHeader.jsx
@@ -4,21 +4,23 @@ import IconCustom from '../../atoms/iconcustom/IconCustom'
 import './cardHeader.css'
 import ModalContext from '../../../../../../core/context/ModalContext'
 import useDetailDelete from '../../../hooks/services/useDetailDelete'
+import Detail from '../../../../infrastructure/models/Details'
 
-function CardHeader({id}) {
+function CardHeader({detail}) {
     const { handleEditerModal } = useContext(ModalContext)
     const deletedDetail = useDetailDelete()
+    
     return (
         <div className='card__header'>
             <IconCustom
                 btnClass="btn-icon"
-                ciconOnclick={handleEditerModal}
+                ciconOnclick={() => handleEditerModal(detail)}
                 icon={icon.cilPencil}
             />
             <IconCustom
                 btnClass="btn-icon red"
                 ciconClass=""
-                ciconOnclick={() => deletedDetail(id)}
+                ciconOnclick={() => deletedDetail(detail[Detail.id])}
                 icon={icon.cilCut}
             />
         </div>
