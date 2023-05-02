e0b1ea7f43bab490ad8f6d337f1ecdda61c98a85
Luis Romero <codesenix@gmail.com>
2023-05-02 05:04:40 -0500

    add isEdit

--- src/domains/personal-accountant/presentation/hooks/useFormDetail.js
+++ src/domains/personal-accountant/presentation/hooks/useFormDetail.js
@@ -1,37 +1,51 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Detail from "../../infrastructure/models/Details"
 import ModalContext from "../../../../core/context/ModalContext"
 import useDetailsCreate from "./services/useDetailCreate"
+import useDetailEdit from "./services/useDetailEdit"
 
 const initialForm = {
     [Detail.id]: "",
     [Detail.name]: "",
+    [Detail.amount]: "0",
     [Detail.detailType]: "0",
-    [Detail.amountOfMoney]: "",
+    [Detail.amountOfMoney]: "0",
     [Detail.description]: "",
+}
+
+const passParamsEdit = (params, setForm) => {
+    const newForm = {
+        [Detail.id]: `${params[Detail.id]}` ?? initialForm[Detail.id],
+        [Detail.name]: params[Detail.name] ?? initialForm[Detail.name],
+        [Detail.amount]: `${params[Detail.amount]}` ?? initialForm[Detail.amount],
+        [Detail.detailType]: `${params[Detail.detailType]}` ?? initialForm[Detail.detailType],
+        [Detail.amountOfMoney]: params[Detail.amountOfMoney] ?? initialForm[Detail.amountOfMoney],
+        [Detail.description]: params[Detail.description] ?? initialForm[Detail.description],
+    }
+    setForm(newForm)
 }
 
 const useFormDetail = () => {
     const [form, setForm] = useState(initialForm)
-    const { closeModal } = useContext(ModalContext)
-    
+    const { closeModal, modalState } = useContext(ModalContext)
     const createdDetail = useDetailsCreate()
+    const editDetail = useDetailEdit()
+    const { params: isEdit } = modalState
+
+    useEffect(() => {
+        if (isEdit) passParamsEdit(isEdit, setForm)
+    }, [modalState])
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        createdDetail(form)
+        isEdit ? editDetail(form) : createdDetail(form)
         setForm(initialForm)
         closeModal()
-        // e.target
-        //   .querySelectorAll(".input")
-        //   .forEach(function ({ name }) {
-        //     console.log(name)
-        //   });
     }
 
-    const handleChange = (event, key) => {
+    const handleChange = (event, key, isMin = 1) => {
         const value = event.target.value
-        setForm({ ...form, [key]: value })
+        if (value.length >= isMin) setForm({ ...form, [key]: value })
     }
 
 
