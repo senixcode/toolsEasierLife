1a8554507e15ef74dc417537926d9a47a7676aa6
Luis Romero <codesenix@gmail.com>
2023-05-02 02:15:51 -0500

    use Detail: CardDetail

--- src/domains/personal-accountant/presentation/components/molecules/card-detail/CardDetail.jsx
+++ src/domains/personal-accountant/presentation/components/molecules/card-detail/CardDetail.jsx
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react'
 import CardHeader from '../card-header/CardHeader'
 import { getTypeDetail } from '../../../utils/getTypeDetail'
+import Detail from '../../../../infrastructure/models/Details';
+
 
 function CardDetail({detail}) {
-   
+
     return (
         <div className='cards__item'>
-           <CardHeader />
+           <CardHeader id={detail[Detail.id]} />
             <div>
-                <h3 className='cards__item-title'>{detail.name}
-                    <small>{getTypeDetail(detail.detailTypeId)}</small>
+                <h3 className='cards__item-title'>{detail[Detail.name]}
+                    <small>{getTypeDetail(detail[Detail.detailType])}</small>
                 </h3>
             </div>
             <p>{detail.description}</p>
