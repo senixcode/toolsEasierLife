2e2f780096c8fc7246efd4e2f8d31feebda3edf3
Luis Romero <codesenix@gmail.com>
2023-05-02 02:17:13 -0500

    add refresh

--- src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
+++ src/domains/personal-accountant/presentation/hooks/services/useDetailsList.js
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query"
 import DetailService from "../../../infrastructure/services/detail/Detail.service"
+import { useState } from "react"
 
 function useDetailsList() {
-    const { status, data: details, error, refetch } = useQuery({
+    const { status, data: details, error, isFetching, refetch } = useQuery({
         queryKey: ['detailsList'],
         queryFn: () => new DetailService().list(),
-        refetchInterval: 1000,
     })
 
-    return { status, details, error, refetch }
+    const refresh = () => setTimeout(function () { refetch() }, 1000)
+    
+    return { status, details, error, isFetching, refresh }
 }
 
 export default useDetailsList
