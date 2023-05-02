410c2e5b0cea1a22d28ba358aa33131ca08e2fb5
Luis Romero <codesenix@gmail.com>
2023-05-02 02:17:52 -0500

    add isFetching

--- src/domains/personal-accountant/presentation/components/organisms/details/Detail.jsx
+++ src/domains/personal-accountant/presentation/components/organisms/details/Detail.jsx
@@ -1,19 +1,18 @@
 import CardDetail from "../../molecules/card-detail/CardDetail";
 import CardSkeleton from "../../molecules/card-skeleton/CardSkeleton";
 import { createListMock } from "../../../../../../shared/infrastructure/adapters/createListMock";
-import useDetailsList from "../../../hooks/services/useDetailsList";
 import { useContext } from "react";
 import DetailContext from "../../../context/DetailContext";
 
 
 function Detail() {
-  const { status, details, error } = useContext(DetailContext)
+  const { status, details, error, isFetching } = useContext(DetailContext)
 
   if (status === 'error') return <span>Error: {error.message}</span>
 
   return (
     <section className='cards__container'>
-      {status === 'loading' ? (
+      {status === 'loading' || isFetching ? (
         createListMock(2).map((_, index) => (
           <CardSkeleton key={index} />
         ))
