a5b29b26b0f207d17e64343b80008fc5dc39cb87
Luis Romero <codesenix@gmail.com>
2023-05-04 19:33:00 -0500

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import * as path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: 'assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: 'core', replacement: path.resolve(__dirname, 'src/core') },
+      { find: 'domains', replacement: path.resolve(__dirname, 'src/domains') },
+      { find: 'shared', replacement: path.resolve(__dirname, 'src/shared') }
+    ]
   },
 })
