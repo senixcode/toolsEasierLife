ee1f29ad0fc60839d452b9e7ba46917bfbd5e102
Luis Romero <codesenix@gmail.com>
2023-05-10 11:13:30 -0500

    add Details ts

--- src/domains/personal-accountant/domain/clases/Details.ts
+++ src/domains/personal-accountant/domain/clases/Details.ts
@@ -1,21 +1,31 @@
-export type TypeDetail = {
-    id?: string
-    name: string
-    detailTypeId: string
-    amount: string
-    amountOfMoney: string
-    description?: string
-    date?: string
+export enum Detail {
+    id = "id",
+    name = "name",
+    detailTypeId = "detailTypeId",
+    amount = "amount",
+    amountOfMoney = "amountOfMoney",
+    description = "description",
+    date = "date"
 }
 
-const Detail = {
-    id: "id",
-    name: "name",
-    detailType: "detailTypeId",
-    amount: "amount",
-    amountOfMoney: "amountOfMoney",
-    description: "description",
-    date: "date"
+export type TypeDetail = {
+    [Detail.id]?: number
+    [Detail.name]: string
+    [Detail.detailTypeId]: number
+    [Detail.amount]: number
+    [Detail.amountOfMoney]: string
+    [Detail.description]?: string
+    [Detail.date]?: string
+}
+
+export type TypeDetailBody = {
+    [Detail.id]?: string
+    [Detail.name]: string
+    [Detail.detailTypeId]: string
+    [Detail.amount]: string
+    [Detail.amountOfMoney]: string
+    [Detail.description]?: string
+    [Detail.date]?: string
 }
 
 export default Detail
